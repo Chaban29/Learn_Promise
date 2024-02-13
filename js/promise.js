52923f48e867a06b2a18d781ac0a610c83520473
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-13 09:45:36 +0200

    learn, and practice promise

--- js/promise.js
+++ js/promise.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { reject } = require("lodash");
+
 const promise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(new Error('Error'));
@@ -46,9 +48,7 @@ const promise4 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error('Error...')), 1000);
 });
 
-promise4
-.then(null, console.log)
-.finally((res) => console.log('finally'));
+promise4.then(null, console.log).finally((res) => console.log('finally'));
 
 const promise5 = new Promise((resolve, reject) => {
   setTimeout(() => resolve('result'), 2000);
@@ -62,8 +62,7 @@ const promise6 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error('Error...')), 1500);
 });
 
-promise6
-  .finally(() => console.log('Promise End'))
+promise6.finally(() => console.log('Promise End'));
 //   .catch((res) => console.log(res));
 
 let promise7 = new Promise((resolve) => resolve('Ok!'));
@@ -236,7 +235,6 @@ const showErrorByPromise = new Promise((res, rej) => {
 
 const loadScript_2 = (src) => {
   return new Promise((res, rej) => {
-
     let script = document.createElement('script');
     script.src = src;
 
@@ -302,7 +300,9 @@ new Promise((res, rej) => {
 
 const promise_7 = fetch('https://jsonplaceholder.typicode.com/users');
 
-promise_7.then((response) => response.json()).then((users) => console.log(users));
+promise_7
+  .then((response) => response.json())
+  .then((users) => console.log(users));
 
 console.log('Request data...');
 
@@ -340,8 +340,8 @@ const sleep = (ms) => {
   });
 };
 
-// sleep(2000).then(() => console.log('After 2 second'));
-// sleep(4000).then(() => console.log('After 4 second'));
+sleep(2000).then(() => console.log('After 2 second'));
+sleep(4000).then(() => console.log('After 4 second'));
 
 Promise.all([sleep(2000), sleep(3000)]).then(() => console.log('All Promises'));
 Promise.race([sleep(2000), sleep(3000)]).then(() =>
@@ -374,3 +374,269 @@ users
     console.log(err);
   })
   .finally(() => console.log('Users is download'));
+
+const promise_10 = new Promise((resolve, reject) => {});
+
+console.log(promise_10);
+
+// Promise States: {
+//   1. Pending,
+//   2. Resolved,
+//   3. Rejected,
+// }
+
+const promise_13 = new Promise((resolve, reject) => {
+  console.log('Loading...');
+  setTimeout(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+      if (response.ok) {
+        resolve(response.json());
+      } else {
+        reject(new Error('Not found 404'));
+      }
+    });
+  }, 3000);
+});
+
+promise_13.then((res) => console.log(res)).catch((err) => console.log(err));
+
+const promise_14 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then((result) => {
+    alert(result);
+    return result * 2;
+  })
+  .then((result) => {
+    alert(result);
+    return result * 2;
+  })
+  .then((result) => {
+    alert(result);
+    return result * 2;
+  });
+
+const promise_15 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then((result) => {
+    alert(1);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then((result) => {
+    alert(result);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then((res) => window.alert(res));
+
+class Thenable {
+  constructor(num) {
+    this.num = num;
+  }
+  then(resolve, reject) {
+    alert(resolve);
+    setTimeout(() => resolve(this.num * 2), 1000);
+  }
+}
+
+new Promise((resolve) => resolve(1))
+  .then((result) => {
+    return new Thenable(result);
+  })
+  .then(alert);
+
+let promise_16 = fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then(console.log);
+
+const promise_12 = new Promise((resolve, reject) => {
+  const isUserLoggedIn = true;
+  if (isUserLoggedIn) {
+    setTimeout(() => resolve('User is loggedIn'), 2000);
+  } else {
+    setTimeout(() => reject('User is not loggedIn'), 2000);
+  }
+});
+
+promise
+  .then((data) => {
+    return new Promise((resolve, reject) => {
+      if (1 > 0) {
+        resolve(data);
+      } else {
+        reject(new Error('Error!'));
+      }
+    });
+  })
+  .then((res) => console.log(res))
+  .catch(console.log);
+
+// new Promise((resolve, reject) => {
+//   reject(new Error('Error!!!'));
+// }).catch(console.log);
+
+// new Promise((resolve, reject) => {
+//   resolve('ok');
+// })
+//   .then(() => {
+//     DashboardRoundedd;
+//   })
+//   .catch(alert);
+
+// new Promise((resolve, reject) => {
+//   throw new Error('Error!');
+// })
+//   .catch((error) => {
+//     if (error instanceof URIError) {
+//     } else {
+//       alert("I don't try this error");
+//       throw error;
+//     }
+//   })
+//   .then(function () {
+//   })
+//   .catch((error) => {
+//     // (**)
+
+//     alert(`Невідома помилка: ${error}`);
+//   });
+
+// window.addEventListener('unhandledrejection', (event) => {
+//   alert(event.promise);
+//   alert(event.reason);
+// });
+
+// new Promise(() => {
+//   throw new Error('Error');
+// });
+
+// fetch('https://no-such-server.blabla')
+//   .then((res) => res.json())
+//   .catch((err) => console.log(err));
+
+// new Promise((resolve, reject) => {
+//   reject(new Error('Помилка!'));
+// }).catch(alert);
+
+// new Promise((resolve, reject) => {
+//   resolve('ok');
+// })
+//   .then(() => {
+//     dsadaaddaada;
+//   })
+//   .catch((err) => console.log(err));
+
+// new Promise((resolve, reject) => {
+//   throw new Error('Error!!!');
+// })
+//   .catch((err) => {
+//     alert(err);
+//   })
+//   .then(() => alert('Управління переходить до наступного обробника then'));
+
+// new Promise((resolve, reject) => {
+//   throw new Error('Error!');
+// })
+//   .catch((err) => {
+//     if (err instanceof URIError) {
+//       alert('URIError');
+//     } else {
+//       alert("I don't worked a error");
+//     }
+//     throw err;
+//   })
+//   .then(function () {})
+//   .catch((error) => {
+//     alert(`Невідома помилка: ${error}`);
+//   });
+
+// window.addEventListener('unhandledrejection', (event) => {
+//   alert(event.promise);
+//   alert(event.reason);
+// });
+
+// new Promise((resolve, reject) => {
+//   throw new Error('Error....');
+// });
+
+// console.log('Start');
+// console.log('Loading...');
+
+// const listPromise = fetch('https://api.sampleapis.com/switch/games')
+//   .then((data) => data.json())
+//   .then((games) => console.log(games));
+
+// // const coffee = new Promise((resolve, reject) => {
+// //   setTimeout(() => {
+// //     resolve('Coffee is done!');
+// //   }, 1500);
+// // }).then((coffee) => console.log(coffee));
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Done!');
+//   }, 1500);
+// })
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// console.log('Start');
+// console.log('Loading...');
+// const usersList = fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((data) => data.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// const coffee = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject(new Error('Your coffee is not already!'));
+//   }, 1500);
+// });
+
+// coffee.then((data) => console.log(data)).catch((error) => console.log(error));
+
+const family = [
+  { member: 'mother', id: 111, coffee: 'Latte' },
+  { member: 'father', id: 222, coffee: 'Espresso' },
+  { member: 'son', id: 333, coffee: 'Cacao' },
+];
+
+const getCoffee = (member) => {
+  const coffeePromise = fetch('https://api.sampleapis.com/coffee/hot');
+  return coffeePromise
+    .then((res) => res.json())
+    .then((list) => {
+      console.log(list);
+      const coffee = list.find((response) => response.title === member.coffee);
+      console.log(coffee);
+      return {
+        ...member,
+        coffee,
+      };
+    });
+};
+
+const getFamilyMember = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const member = family.find((response) => response.id === id);
+      if (member) {
+        resolve(member);
+      } else {
+        reject(new Error('Member is not defined'));
+      }
+    }, 1500);
+  });
+};
+
+getFamilyMember(222)
+  .then((data) => {
+    return getCoffee(data);
+  })
+  .then((newMember) => {
+    console.log(newMember);
+  });
